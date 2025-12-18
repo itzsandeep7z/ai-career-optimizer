be86c32ce251e2a9012ac3e72dd169b9658cf9d1
teamrookies <runtimerookies881@gmail.com>
2025-12-18 22:10:03 +0530

    server.js

--- ai-career-optimizer/backend/server.js
+++ ai-career-optimizer/backend/server.js
@@ -7,19 +7,19 @@ const enhanceRoutes = require("./routes/enhance");
 const interviewRoutes = require("./routes/interview");
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("Backend running");
+  res.send("AI Career Optimizer Backend Running");
 });
 
 app.use("/api/resume", resumeRoutes);
-app.use("/api/resume", enhanceRoutes);
+app.use("/api/enhance", enhanceRoutes);
 app.use("/api/interview", interviewRoutes);
 
 const PORT = process.env.PORT || 5000;
-
 app.listen(PORT, () => {
   console.log("Server started on port " + PORT);
 });
